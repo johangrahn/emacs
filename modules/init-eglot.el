c912ab23e10a558cbc24f82eceba64b3e128b607
Johan Grahn <johan.grahn@me.com>
2023-09-18 08:54:30 +0200

    Use tree-sitter from Emacs builtin
    This makes the use of `js-ts-mode` instead of `js-mode`

--- modules/init-eglot.el
+++ modules/init-eglot.el
@@ -4,8 +4,10 @@
   (elixir-mode . eglot-ensure)
   (before-save . eglot-format-buffer)
   (js2-mode . eglot-ensure)
+  (js-ts-mode . eglot-ensure)
   :config
   (setq js-indent-level 2)
   (setq js2-basic-offset 2))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-ts-mode))
 
 (provide 'init-eglot)
